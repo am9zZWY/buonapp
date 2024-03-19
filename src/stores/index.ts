5f3ba027dd177febb638f96c44040fd4543f9ca3
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-03-19 22:09:04 +0100

    Add useful metadata and remove unused files

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -11,8 +11,23 @@ type Recipe = {
 
 export const useRecipeStore = defineStore('recipes', () => {
     const recipes = ref<Recipe[]>([])
+    const BASE_URL = 'https://tastebuddy-1-k6629823.deta.app/'
 
-    // TODO: fetch recipes from the server
+    // Fetch recipes from the server
+    fetch(`${BASE_URL}recipes`).then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok')
+        }
+
+        return response
+    }).then((response) => {
+        return response.json()
+    }).then((data) => {
+        recipes.value = data
+    }).catch((error) => {
+        console.error('There has been a problem with your fetch operation:', error)
+    })
+
     return {recipes}
 })
 
