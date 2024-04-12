56cbbd9b5f596b01472bad90d0edcac040a98660
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-12 11:53:50 +0200

    Fix tailwind class

--- src/stories/thumbnail-link/ThumbnailLinkContainer.vue
+++ src/stories/thumbnail-link/ThumbnailLinkContainer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mb-14">
         <h1 class="text-xl font-serif font-bold">{{ title }}</h1>
-        <h2 class="text-lg font-serif font-extraligh text-gray-800" v-if="subtitle">{{ subtitle }}</h2>
+        <h2 class="text-lg font-serif font-extralight text-gray-800" v-if="subtitle">{{ subtitle }}</h2>
         <div class="pt-5 flex-wrap justify-start gap-x-20 gap-y-10 flex">
             <slot></slot>
         </div>
