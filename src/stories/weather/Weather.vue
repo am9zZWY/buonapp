d980360d5e1aa8bc3173234ec995a681a7ca54ed
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-12 11:43:27 +0200

    Use real weather data

--- src/stories/weather/Weather.vue
+++ src/stories/weather/Weather.vue
@@ -1,33 +1,60 @@
 <template>
     <div>
-        {{ location }} - {{ temperature }}°C - {{ weather }}
+        <button @click="editLocation" v-if="!edit.location">
+            {{ location }}
+        </button>
+        <input v-show="edit.location" v-model="location" @blur="saveLocation" ref="locationInput"/>
+        – {{ temperature }}°C – {{ weather }}
     </div>
 </template>
 
 <script lang="ts" setup>
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
 
 type WeatherProps = {
     location: string
 }
 
+type Edit = { [key: string]: boolean }
+const edit = ref<Edit>({})
+
 const props = withDefaults(defineProps<WeatherProps>(), {
-    location: 'Milan, Italy'
+    location: 'Posada, Italy'
 })
 
+const location = ref(props.location)
 const temperature = ref(15)
 const weather = ref('Sunny')
+const locationInput = ref(null)
 
-/*
-TODO:
-onMounted(() => {
+const editLocation = () => {
+    edit.value.location = true
+    locationInput?.value?.focus()
+}
+const saveLocation = () => {
+    edit.value.location = false
+    fetchWeather(location.value)
+}
+
+const fetchWeather = async (location: string) => {
+    if (!location || location === '') {
+        return
+    }
+
     fetch(`https://api.weatherapi.com/v
-1/current.json?key=YOUR_API_KEY&q=${props.location}`)
+1/current.json?key=ed0259613b1d44d387890933241204&q=${location}`)
         .then((response) => response.json())
         .then((data) => {
             temperature.value = data.current.temp_c
             weather.value = data.current.condition.text
         })
-})
- */
+}
+
+watch(location, () => {
+    if (edit.value.location) {
+        return
+    }
+    fetchWeather(location.value)
+}, {immediate: true})
+
 </script>
