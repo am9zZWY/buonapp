0f5c2523404a92b18481f4405378f0b38766bdb7
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-12 14:20:25 +0200

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,41 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import {VitePWA, VitePWAOptions} from 'vite-plugin-pwa'
 import vue from '@vitejs/plugin-vue'
+
+const pwaOptions: Partial<VitePWAOptions> = {
+    base: '/',
+    devOptions: {
+        enabled: process.env.SW_DEV === 'true',
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+    },
+    includeAssets: ['favicon.svg'],
+    manifest: {
+        name: 'Buonapp: Your daily helper',
+        short_name: 'Buonapp',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait'
+    },
+    mode: 'development',
+    registerType: 'autoUpdate',
+    selfDestroying: true,
+    minify: true
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA(pwaOptions)
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 })
