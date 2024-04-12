161897f0a487972163e56c91eaa87c762774fd30
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-12 22:58:51 +0200

    Improve weather component

--- src/stories/weather/Weather.vue
+++ src/stories/weather/Weather.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
-        <button @click="editLocation" v-if="!edit.location" :disabled="disableEdit">
-            {{ location }}
+        <button @click="editLocation" v-if="!edit.location" :disabled="disableEdit" class="hover:underline focus:outline-none">
+            {{ weather.location }}
         </button>
-        <input v-show="edit.location" v-model="locationInput" @blur="saveLocation" ref="locationInputEl"/>
-        – {{ temperature }}°C – {{ weather }}
+        <input v-show="edit.location" v-model="locationInput" @blur="saveLocation" @keydown.enter="saveLocation" ref="locationInputEl" class="border-b border-gray-300 italic" />
+        – {{ weather.temperature }}°C – {{ weather.weather }}
+    </div>
+    <div class="text-gray-600 font-extralight text-xs">
+        Last updated: {{ weather.lastUpdated }}
     </div>
 </template>
 
 <script lang="ts" setup>
-import {computed, ref} from 'vue'
+import {computed, ref, watch} from 'vue'
 import {useWeatherStore} from "@/stores";
 
 type WeatherProps = {
-    disableEdit?: boolean
+    disableEdit?: boolean,
 }
 
 type Edit = { [key: string]: boolean }
@@ -25,19 +28,22 @@ withDefaults(defineProps<WeatherProps>(), {
 
 const weatherStore = useWeatherStore()
 
-const location = computed(() => weatherStore.weather.location)
-const temperature = computed(() => weatherStore.weather.temperature)
-const weather = computed(() => weatherStore.weather.weather)
-const locationInput = ref(weatherStore.weather.location)
+const weather = computed(() => weatherStore.weather)
+const locationInput = ref<string>('')
 const locationInputEl = ref(null)
 
 
 const editLocation = () => {
     edit.value.location = true
-    locationInputEl?.value?.focus()
+    locationInputEl.value?.focus()
+    console.log('editLocation', locationInputEl.value)
 }
 const saveLocation = () => {
     edit.value.location = false
     weatherStore.updateLocation(locationInput.value)
 }
+
+watch(() => weather.value.location, () => {
+    locationInput.value = weather.value.location
+}, {immediate: true})
 </script>
