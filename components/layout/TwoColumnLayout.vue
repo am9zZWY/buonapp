a0ac5470f32ac12d49be48fb3ff2e582e74a4c03
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-14 02:04:11 +0200

    Remove recipe component for now

--- components/layout/TwoColumnLayout.vue
+++ components/layout/TwoColumnLayout.vue
@@ -3,81 +3,66 @@
   -->
 
 <template>
-    <IonGrid>
-        <IonRow>
-            <IonCol v-if="$slots.left" :size-lg="selectedLayout.left.lg"
-                    :size-md="selectedLayout.left.md"
-                    :size-sm="$slots.right ? selectedLayout.left.sm : '12'"
-                    :size-xl="selectedLayout.left.xl"
-                    class="left"
-                    size="12"
-                    v-bind="$props">
-                <slot name="left"/>
-            </IonCol>
+  <div class="flex">
+    <div v-if="$slots.left" class="left">
+      <slot name="left" />
+    </div>
 
-            <IonCol v-if="$slots.right" :size-lg="$slots.left ? selectedLayout.right.lg : '12'"
-                    :size-md="$slots.left ? selectedLayout.right.md : '12'"
-                    :size-xl="$slots.left ? selectedLayout.right.xl : '12'"
-                    class="right"
-                    size="12"
-                    size-sm="12"
-                    v-bind="$props">
-                <slot name="right"/>
-            </IonCol>
-        </IonRow>
-    </IonGrid>
+    <div v-if="$slots.right" class="right">
+      <slot name="right" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, PropType, toRefs, useSlots } from 'vue';
-import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+import { computed, type PropType, toRefs, useSlots } from 'vue'
 
 const props = defineProps({
-    layout: {
-        type: String as PropType<'default' | 'leftBigger' | 'rightBigger' | 'noRight'>,
-        required: false,
-        default: 'default'
-    }
+  layout: {
+    type: String as PropType<'default' | 'leftBigger' | 'rightBigger' | 'noRight'>,
+    required: false,
+    default: 'default'
+  }
 })
-const {layout} = toRefs(props)
+const { layout } = toRefs(props)
 
 const slots = useSlots()
 const hasRightSlot = computed(() => !!slots['right'])
 
 type Layout = {
-    left: {
-        xl: string, lg: string, md: string, sm: string
-    }, right: {
-        xl: string, lg: string, md: string, sm: string
-    }
+  left: {
+    xl: string, lg: string, md: string, sm: string
+  }, right: {
+    xl: string, lg: string, md: string, sm: string
+  }
 }
 
 const layouts: { [key: string]: Layout } = {
-    default: {
-        left: {
-            xl: '6', lg: '6', md: '6', sm: '6'
-        }, right: {
-            xl: '6', lg: '6', md: '6', sm: '6'
-        }
-    }, leftBigger: {
-        left: {
-            xl: '8', lg: '8', md: '7', sm: '6'
-        }, right: {
-            xl: '4', lg: '4', md: '5', sm: '6'
-        }
-    }, rightBigger: {
-        left: {
-            xl: '4', lg: '4', md: '5', sm: '6'
-        }, right: {
-            xl: '8', lg: '8', md: '7', sm: '6'
-        }
-    }, noRight: {
-        left: {
-            xl: '12', lg: '12', md: '12', sm: '12'
-        }, right: {
-            xl: '0', lg: '0', md: '0', sm: '0'
-        }
+  default: {
+    left: {
+      xl: '6', lg: '6', md: '6', sm: '6'
+    }, right: {
+      xl: '6', lg: '6', md: '6', sm: '6'
     }
+  }, leftBigger: {
+    left: {
+      xl: '8', lg: '8', md: '7', sm: '6'
+    }, right: {
+      xl: '4', lg: '4', md: '5', sm: '6'
+    }
+  }, rightBigger: {
+    left: {
+      xl: '4', lg: '4', md: '5', sm: '6'
+    }, right: {
+      xl: '8', lg: '8', md: '7', sm: '6'
+    }
+  }, noRight: {
+    left: {
+      xl: '12', lg: '12', md: '12', sm: '12'
+    }, right: {
+      xl: '0', lg: '0', md: '0', sm: '0'
+    }
+  }
 }
 
 const selectedLayout = computed(() => hasRightSlot.value ? layouts[layout.value] : layouts['noRight'])
@@ -85,26 +70,26 @@ const selectedLayout = computed(() => hasRightSlot.value ? layouts[layout.value]
 
 <style>
 .left {
-    margin: 0;
-    padding-left: 0;
-    padding-right: 1rem;
+  margin: 0;
+  padding-left: 0;
+  padding-right: 1rem;
 }
 
 .right {
-    margin: 0;
-    padding-left: 1rem;
-    padding-right: 0;
+  margin: 0;
+  padding-left: 1rem;
+  padding-right: 0;
 }
 
 @media (max-width: 768px) {
-    .left {
-        padding-left: 0;
-        padding-right: 0;
-    }
+  .left {
+    padding-left: 0;
+    padding-right: 0;
+  }
 
-    .right {
-        padding-left: 0;
-        padding-right: 0;
-    }
+  .right {
+    padding-left: 0;
+    padding-right: 0;
+  }
 }
 </style>
