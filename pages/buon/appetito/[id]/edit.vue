40ca2c621d8816c9846e905621951446ccab2bb8
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-18 00:19:35 +0200

    Improve recipe and editor

---
+++ pages/buon/appetito/[id]/edit.vue
@@ -0,0 +1,15 @@
+<template>
+  <RecipeEditor :id="id" no-redirect />
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+const id = computed(() => {
+  const id = route.params.id as string
+  if (!id) {
+    return null
+  }
+
+  return id
+})
+</script>
--- pages/buon/appetito/index.vue
+++ pages/buon/appetito/index.vue
@@ -2,17 +2,20 @@
   <!-- Add recipes -->
   <Section title="Ricetta del giorno" subtitle="Today we recommend" no-shadow>
     <template #full-width>
-      <ThumbnailHero :title="recipes?.[0]?.name" subtitle="La ricetta più semplice e veloce" :description="recipes?.[0]?.instructions" :to="'/buon/appetito/' + recipes?.[0]?.id" :src="recipes?.[0]?.images" />
+      <ThumbnailHero :title="recipeList?.[0]?.name" subtitle="La ricetta più semplice e veloce" :description="recipeList?.[0]?.instructions" :to="'/buon/appetito/' + recipeList?.[0]?.id" :src="recipeList?.[0]?.images" />
     </template>
   </Section>
 
   <Section title="Cosa cucinare oggi? 🍝" subtitle="What to cook today?">
-    <ThumbnailLink v-for="recipe in recipes" :key="recipe.id" :title="recipe.name" :description="recipe.text"
-                   :eyebrow="recipe.instructions.split('\n').length + ' steps'" :to="'/buon/appetito/' + recipe.id">
+    <ThumbnailLink v-for="recipe in recipeList" :key="recipe.id" :title="recipe.name" :description="recipe.text"
+                   :eyebrow="recipe.instructions.split('\n').length + ' steps'" :to="'/buon/appetito/' + recipe.id"
+                    :src="recipe.images?.split(',')[0]">
     </ThumbnailLink>
   </Section>
 
-  <Button label="Add your own recipe" to="/buon/appetito/add" />
+  <Section title="Personalizza la tua esperienza" subtitle="Customize your experience">
+    <Button label="Add your own recipe" to="/buon/appetito/add" />
+  </Section>
 </template>
 
 <script lang="ts" setup>
@@ -20,5 +23,5 @@ import { useRecipeStore } from '~/stores/recipe'
 import ThumbnailHero from '~/components/thumbnail/ThumbnailHero.vue'
 
 const recipeStore = useRecipeStore()
-const { recipes } = storeToRefs(recipeStore)
+const recipeList = computed(() => recipeStore.recipeList)
 </script>
