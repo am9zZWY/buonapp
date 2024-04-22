7aa06debccd6a5a09edc303e59bcff3f17ed2bad
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-04-22 15:38:05 +0200

    Replace pwa manifest with pwa assets

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mx-5">
-    <NuxtPwaManifest />
+    <NuxtPwaAssets />
     <NuxtLoadingIndicator />
     <NuxtLayout>
       <NuxtPage />
@@ -11,17 +11,17 @@
 <script setup lang="ts">
 useSeoMeta({
   title: 'Buonapp - Your daily companion ☀️',
-  description: "Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!",
-  author: "Josef Müller 🚀",
-  robots: "index, follow",
-  ogTitle: "Buonapp - Your daily companion ☀️",
-  ogDescription: "Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!",
-  ogUrl: "https://buonapp.vercel.app/buonapp",
-  ogImage: "https://buonapp.vercel.app/logo.png",
-  twitterTitle: "Buonapp - Your daily companion ☀️",
-  twitterDescription: "Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!",
-  twitterImage: "https://example.com/buonapp.jpg"
-});
+  description: 'Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!',
+  author: 'Josef Müller 🚀',
+  robots: 'index, follow',
+  ogTitle: 'Buonapp - Your daily companion ☀️',
+  ogDescription: 'Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!',
+  ogUrl: 'https://buonapp.vercel.app/buonapp',
+  ogImage: 'https://buonapp.vercel.app/logo.png',
+  twitterTitle: 'Buonapp - Your daily companion ☀️',
+  twitterDescription: 'Discover buonapp: Your ultimate all-in-one tool for seamless daily management. With features like calendar, todos, recipes, weather, and news, buonapp streamlines your day with simplicity and convenience. Say hello to effortless organization and productivity!',
+  twitterImage: 'https://example.com/buonapp.jpg'
+})
 
 
 </script>
