da6cf4384376083543390225e0bcb184f8edde13
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-05-08 22:06:36 +0200

    Integrate openai into rss

--- server/api/rss/index.get.ts
+++ server/api/rss/index.get.ts
@@ -1,18 +1,25 @@
+import type { XmlDocument } from '@rgrove/parse-xml'
 import { parseXml } from '@rgrove/parse-xml'
-import { Rss } from '~/types/rss'
+import type { Rss } from '~/types/rss'
+import OpenAI from 'openai'
+import type { NitroRuntimeConfig } from 'nitropack'
 
 const news_urls = [
   'https://www.ansa.it/sito/notizie/topnews/topnews_rss.xml',
   'https://www.tagesschau.de/xml/rss2',
-  'https://www.aljazeera.com/xml/rss/all.xml',
+  'https://www.aljazeera.com/xml/rss/all.xml'
 ]
 
-async function getNewsFeed() {
+/**
+ * Get news feed from multiple sources
+ * @returns {Promise<Rss[]>} List of news items
+ */
+async function getNewsFeed(): Promise<Rss[]> {
   const requests = news_urls.map(url =>
     fetch(url)
       .then(response => response.text())
       .then((data: string) => {
-        const document = parseXml(data)
+        const document: XmlDocument = parseXml(data)
         if (!document) {
           return []
         }
@@ -36,6 +43,35 @@ async function getNewsFeed() {
     .then(data => data.toSorted((a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime()))
 }
 
-export default defineEventHandler(async (event) => {
+async function getCompactNewsFeed(): Promise<string> {
   return getNewsFeed()
+    .then(news => news.map(item => `${item.title}\n${item.description}`).join('\n\n'))
+}
+
+export default defineEventHandler(async (event) => {
+  const config: NitroRuntimeConfig = useRuntimeConfig(event)
+  const { openaiApiKey } = config
+  if (!openaiApiKey) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'OpenAI API key not provided'
+    })
+  }
+
+  const openai = new OpenAI({ apiKey: openaiApiKey })
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: 'What is the summary of the latest news in 3 sentences?' },
+      { role: 'system', content: await getCompactNewsFeed() }
+    ],
+    max_tokens: 100,
+    n: 1,
+    stop: ['\n'],
+    temperature: 0.5
+  })
+
+  return completion.choices[0].message.content
 })
