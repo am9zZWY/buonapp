175aca8bf5d912a5f3cb036a437b32fb0e3cb5d2
am9zZWY <46693545+am9zZWY@users.noreply.github.com>
2024-05-24 02:40:22 +0200

    Remove recipes
    Signed-off-by: am9zZWY <46693545+am9zZWY@users.noreply.github.com>

--- pages/buon/appetito/add.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <RecipeEditor />
-</template>
--- pages/index.vue
+++ pages/index.vue
@@ -17,22 +17,6 @@
     <Section title="Plan your day 📅">
       <TodoContainer />
     </Section>
-
-    <Section title="️Cosa cucinare oggi? 🍝" subtitle="What to cook today?">
-      <ClientOnly>
-        <ThumbnailLink
-          alt="Cooking Image"
-          description=""
-          src="https://t4.ftcdn.net/jpg/03/32/75/39/240_F_332753934_tBacXEgxnVplFBRyKbCif49jh0Wz89ns.jpg"
-          title="Search for new recipes"
-          to="/buon/appetito"
-        >
-          <template #description>
-            <p>Recipes and delicious dishes!</p>
-          </template>
-        </ThumbnailLink>
-      </ClientOnly>
-    </Section>
   </div>
 </template>
 
